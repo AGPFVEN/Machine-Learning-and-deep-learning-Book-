a1d85325c2703ddc16d67a1316559ff51a824395
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-06-26 23:57:06 +0200

    Print features

--- Ch2 (Datasets)/Dataset de diabetes (regresión).py
+++ Ch2 (Datasets)/Dataset de diabetes (regresión).py
@@ -10,10 +10,12 @@ age = 0
 sex = 1
 blood_pressure = 3
 
-#print(diabetes.DESCR) #Text information about this dataset
+print(diabetes.DESCR) #Text information about this dataset
 
 print(X.shape)         #Contains 442 samples (rows) of 10 features (columns)
 print(Y.shape)         #Adequate for Regreession. The target are numbers
 
 print(X[0:5, :])       #Print rows of data, starting from 0 to 4
 print(Y[0:5])          #Print rows of target data, starting from 0 to 4
+
+print(X[1, 2])         #Print mass index (third column)
