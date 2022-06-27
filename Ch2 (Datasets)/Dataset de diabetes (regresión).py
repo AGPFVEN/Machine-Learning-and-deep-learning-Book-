da2db1c649c4770efcda0bf93efe4c009d8fc0bd
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-06-27 15:02:49 +0200

    Print a feature of multiple samples

--- Ch2 (Datasets)/Dataset de diabetes (regresión).py
+++ Ch2 (Datasets)/Dataset de diabetes (regresión).py
@@ -8,9 +8,10 @@ Y = diabetes.target
 
 age = 0
 sex = 1
+mass_index = 2
 blood_pressure = 3
 
-print(diabetes.DESCR) #Text information about this dataset
+#print(diabetes.DESCR) #Text information about this dataset
 
 print(X.shape)         #Contains 442 samples (rows) of 10 features (columns)
 print(Y.shape)         #Adequate for Regreession. The target are numbers
@@ -18,4 +19,5 @@ print(Y.shape)         #Adequate for Regreession. The target are numbers
 print(X[0:5, :])       #Print rows of data, starting from 0 to 4
 print(Y[0:5])          #Print rows of target data, starting from 0 to 4
 
-print(X[1, 2])         #Print mass index (third column)
+print(X[1, mass_index])#Print mass index (third column)
+print(X[1:3, age])       #Print age of second and third samples (first column)
